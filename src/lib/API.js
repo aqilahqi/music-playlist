27c45849d0f85f3fe67da97241a91e9d1f4a9166
Aqilah <aqilahfatin01@gmail.com>
2023-03-07 16:18:07 +0800

    add store for music

--- src/lib/API.js
+++ src/lib/API.js
@@ -4,7 +4,20 @@ const api = axios.create({
   baseURL: 'https://api.discogs.com'
 })
 
+const consumer_key = 'nidCIjZGeKILBIkVrnkP'
+const consumer_secret = 'IkFrxnmDwjoXBshzklGSdOFVhUDOWUAW'
+const timestamp = new Date.now()
+
+export async function requestToken() {
+  const { oauth_token, oauth_token_secret } = await api.get('/oauth/request_token', {
+    headers: {
+      Authorization: `OAuth oauth_consumer_key="${consumer_key}",oauth_signature_method="PLAINTEXT",oauth_timestamp="${timestamp}",oauth_nonce="31f1c3e2-acfe-4f62-b4eb-70d4b62e4c15",oauth_version="1.0",oauth_signature="${consumer_secret}&", oauth_callback="your_callback"`
+    }
+  })
+  return oauth_token, oauth_token_secret
+}
+
 export async function getReleases() {
-  const { data } = await api.get('/artists/21262/releases')
-  return data
+  const { pagination, releases } = await api.get('/artists/21262/releases')
+  return pagination, releases
 }
--- src/stores/music.js
+++ src/stores/music.js
@@ -1,1 +1,46 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('music', () => {
+  const releases = ref([])
+  const favourites = ref([])
+
+  function displayReleases(arr) {
+    releases.value = []
+
+    const newMusicList = arr.map((a) => {
+      if (favourites.value.length > 0) {
+        favourites.value.map((f) => {
+          if (a.id === f.id) {
+            a = Object.assign(a, { favourite: true })
+          } else {
+            a = Object.assign(a, { favourite: false })
+          }
+        })
+      } else {
+        a = Object.assign(a, { favourite: false })
+      }
+    })
+
+    releases.value.concat(...newMusicList)
+  }
+  function toggleFavourites(obj) {
+    if (releases.value.length > 0) {
+      releases.value.map((release) => {
+        if (release.id === obj.id) {
+          release.favourite = !release.favourite
+        }
+      })
+    }
+    if (favourites.value.length > 0) {
+      favourites.value.map((f) => {
+        if (obj.id === f.id) {
+          obj.favourite = !obj.favourite
+        }
+      })
+    }
+  }
+
+  return { releases, favourites, displayReleases, toggleFavourites }
+})
